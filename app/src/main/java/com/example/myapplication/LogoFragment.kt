f3f78f0eb59df726b98e43d9f6a833a75ab0dd36
SantiagoS <santiagosanchezmon@gmail.com>
2022-09-05 22:07:29 -0300

    cambio de logo, correccion de vista login

--- app/src/main/java/com/example/myapplication/LogoFragment.kt
+++ app/src/main/java/com/example/myapplication/LogoFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myapplication.databinding.FragmentLogoBinding
+import com.example.myapplication.databinding.FragmentLoginBinding
 
 class LogoFragment : Fragment() {
 
-    private var _binding: FragmentLogoBinding? = null
-    private val binding: FragmentLogoBinding get() = _binding!!
+    private var _binding: FragmentLoginBinding? = null
+    private val binding: FragmentLoginBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding =  FragmentLogoBinding.inflate(inflater, container, false)
+        _binding =  FragmentLoginBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
