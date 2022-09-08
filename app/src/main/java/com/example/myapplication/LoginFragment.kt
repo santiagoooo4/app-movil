9eb0bd52b5ecd60bceeeb4d408f883f84066da74
Luis Mejia <luismejia69@gmail.com>
2022-09-08 16:31:03 -0500

    register layout xml and kt

--- app/src/main/java/com/example/myapplication/LoginFragment.kt
+++ app/src/main/java/com/example/myapplication/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -21,11 +22,25 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
-//    override fun onStart() {
-//        super.onStart()
-//
-//        binding.fragmentLoginBottom.setOnClickListener {
-//            if (!binding.loginEmailLayout.text.toString().isValidEmail())
-//        }
-//    }
+    override fun onStart() {
+        super.onStart()
+
+        binding.fragmentLoginBottom.setOnClickListener {
+            if (!binding.loginEmail.text.toString().isValidEmail()){
+                binding.loginEmailLayout.error = getString(R.string.error_email)
+            } else {
+                binding.loginEmailLayout.error = null
+            }
+
+            if (!binding.loginPassword.text.toString().isValidPassword()){
+                    binding.passwdLoginLayout.error = getString(R.string.invalid_password)
+                } else {
+                binding.passwdLoginLayout.error = null
+            }
+
+            binding.fragmentLoginBottom.setOnClickListener {
+                findNavController().navigate(R.id.action_logoFragment_to_registerFragment)
+            }
+        }
+   }
 }
---
+++ app/src/main/java/com/example/myapplication/RegisterFragment.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentRegisterBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RegisterFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RegisterFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding: FragmentRegisterBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
